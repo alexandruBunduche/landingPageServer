cf361548f285cac59f22af3527d9b696e061f00a
Alexandru Bunduche <alexandru.bunduche@daleconsulting.it>
2018-10-09 14:59:23 +0200

    init

--- landingPageServer/routes/index.js
+++ landingPageServer/routes/index.js
@@ -1,10 +1,8 @@
-﻿'use strict';
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
+/*
+ * GET home page.
+ */
 
-module.exports = router;
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
