5586ef14349c599303713f1f4b1a2c9363470364
Alexandru Bunduche <alexandru.bunduche@daleconsulting.it>
2018-10-16 14:01:34 +0200

    fix

--- landingPageServer/app.js
+++ landingPageServer/app.js
@@ -27,19 +27,6 @@ app.get('/qualifications', (req, res) => getQualificationTypes(req, res))
 
 app.post('/candidate', (req, res) => postCandidate(req, res))
 
-app.get('/', (req, res) => getIndexPage(req, res))
-
-app.get('/runtime.js', (req, res) => getRuntime(req, res))
-app.get('/polyfills.js', (req, res) => getPolifills(req, res))
-app.get('/main.js', (req, res) => getMain(req, res))
-app.get('/styles.js', (req, res) => getStyles(req, res))
-app.get('/vendor.js', (req, res) => getVendor(req, res))
-app.get('/favicon.ico', (req, res) => getFavicon(req, res))
-app.get('/open-sans-v15-latin-regular.woff2', (req, res) => getOpenSansV15LatinRegular2(req, res))
-app.get('/primeicons.ttf', (req, res) => getPrimeiconsTtf(req, res))
-app.get('/open-sans-v15-latin-regular.woff', (req, res) => getOpenSansV15LatinRegular(req, res))
-app.get('/primeicons.woff', (req, res) => getPrimeicons(req, res))
-app.get('/open-sans-v15-latin-regular.ttf', (req, res) => getOpenSansV15LatinRegularTtf(req, res))
 
 app.listen(port);
 
@@ -59,58 +46,4 @@ function postCandidate(req, res){
     candidateModel.create(req.body)
         .then(() => res.status(200).send())
         .catch(err => res.status(506).send(errors.emailAlreadyPresent))
-    
-}
-
-function getIndexPage(req, res){
-  res.sendfile('./public/index.html');
-}
-
-
-function getRuntime(req, res){
-  res.sendfile('./public/runtime.js');
-}
-
-
-function getPolifills(req, res){
-  res.sendfile('./public/polyfills.js');
-}
-
-
-function getMain(req, res){
-  res.sendfile('./public/main.js');
-}
-
-function getStyles(req, res){
-  res.sendfile('./public/styles.js');
-}
-
-function getVendor(req, res){
-  res.sendfile('./public/vendor.js');
-}
-
-
-
-function getFavicon(req, res){
-  res.sendfile('./public/favicon.ico');
-}
-
-function getOpenSansV15LatinRegular2(req, res){
-  res.sendfile('./public/open-sans-v15-latin-regular.woff2');
-}
-
-function getPrimeiconsTtf(req, res){
-  res.sendfile('./public/primeicons.ttf');
-}
-
-function getOpenSansV15LatinRegular(req, res){
-  res.sendfile('./public/open-sans-v15-latin-regular.woff');
-}
-
-function getPrimeicons(req, res){
-  res.sendfile('./public/primeicons.woff');
-}
-
-function getOpenSansV15LatinRegularTtf(req, res){
-  res.sendfile('./public/open-sans-v15-latin-regular.ttf');
 }
